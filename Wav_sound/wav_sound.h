398786d86f27565a5671fd474d477902329d2b66
Mashenka737 <kataeva.Maria737@yandex.ru>
2016-03-05 00:57:03 +0300

    There is an approximately structure of our progect

---
+++ Wav_sound/wav_sound.h
@@ -0,0 +1,32 @@
+#ifndef WAV_SOUND_H
+#define WAV_SOUND_H
+
+#include<iostream>
+#include<vector>
+
+class WavFile
+{
+public:
+    WavFile ();
+    ~WavFile ();
+    int openWavFile (const char* fileName);
+    int initialize ();
+    int getAmplArray (std::vector<int> &ampl);
+    int getTimeDiff ();
+    int closeWavFile ();
+private:
+    int diffTime;
+};
+
+struct AmplFrecArray
+{
+    std::vector<int> ampl;
+    int diffFrec;
+    int diffTime;
+    int ntime;
+};
+
+AmplFrecArray DpfTransform (const std::vector<int> &Ampl, int diffTime, int sizeWindow);
+
+
+#endif // WAV_SOUND_H
