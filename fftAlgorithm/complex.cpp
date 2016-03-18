545d54de2ca5f62af91793e4965c364c2ed13d1c
Irina Nikulina <washburn12560@gmail.com>
2016-03-18 23:59:30 +0300

    fftAlgorithm
    These files can be used to transform array of complex data into array of
    amplitudes (double) using fft.

---
+++ fftAlgorithm/complex.cpp
@@ -0,0 +1,26 @@
+//   complex.cpp - impelementation of class
+//   of complex number
+//
+//   The code is property of LIBROW
+//   You can use it on your own
+//   When utilizing credit LIBROW site
+
+//   Include header file
+#include "complex.h"
+#include <iostream>
+//#include <QTextStream>
+
+
+//   Imaginary unity constants
+const complex complex::i(0., 1.);
+const complex complex::j(0., 1.);
+
+void complex::print()
+{
+    //cout << "this is" << m_re <<  "+" << m_im << "i";
+   // QTextStream cout(stdout);
+   std::cout << "re= " << m_re << " im = " << m_im << "\n"  ;
+   //std::endl;
+
+}
+
