4f101909eb824952c43ac684808d41f9027e482a
Alexander Surchenko <surchenko.a@mail.ru>
2016-03-23 22:36:01 +0300

    Adding song_window
    Adding new window for displaying previously uploaded songs.. Adding new
    button, adjusting buttons' length. Adding new logo, creating two Fixed
    (one for logo, another for buttons), deleting old one.

--- Interface/Develop/main.cpp
+++ Interface/Develop/main.cpp
@@ -9,13 +9,17 @@
 int main() {
 
 	const sf::Vector2f NOTE_WINDOW_SCR_REQ = {900.0f, 480.0f};
-	const sf::Vector2f NOTE_WINDOW_POS = {330.0f, 125.0f};
+	const sf::Vector2f NOTE_WINDOW_POS = {330.0f, 100.0f};
 	const sf::Vector2f NOTE_WINDOW_REQ = {900.0f, 500.0f};
+	const sf::Vector2f SONG_WINDOW_SCR_REQ = {250.0f, 555.0f};
+	const sf::Vector2f SONG_WINDOW_POS = {25.0f, 100.0f};
+	const sf::Vector2f SONG_WINDOW_REQ = {250.0f, 575.0f};
 	const sf::Vector2f ENTRY_REQ = {700.0f, 25.0f};
 	const int BUTTON_FONT_SIZE = 24;
-	const sf::Vector2f START_CORD = {250.0f, 650.0f};
-	const sf::Vector2f CANCEL_CORD = {400.0f, 650.0f};
-	const sf::Vector2f LOGO_CORD = {450.0f, .0f};
+	const sf::Vector2f START_COORD = {125.0f, 550.0f};
+	const sf::Vector2f CANCEL_COORD = {350.0f, 550.0f};
+	const sf::Vector2f GETIMAGE_COORD = {575.0f, 550.0f};
+	const sf::Vector2f LOGO_COORD = {.0f, .0f};
 	const sf::Vector2f WINDOW_SIZE = {1280.0f, 720.f};
 	const sf::VideoMode BASE_SIZE = {1280, 720};
 
@@ -47,10 +51,11 @@ int main() {
 	sf::Image logo;
 	auto logo_image = sfg::Image::Create();
 
-	if( logo.loadFromFile( "E:/Programs/Qt/Projects/sfml/images/mipt_logo.png" ) ) {
+	if( logo.loadFromFile( "E:/Programs/Qt/Projects/sfml/images/mipt_logo_long.png" ) ) {
 			logo_image->SetImage( logo );
 	}
 
+	//Editing note window
 	auto note_window_scr_box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL);
 	note_window_scr_box->Pack(note_image, true, true);
 
@@ -69,6 +74,32 @@ int main() {
 
 	note_window->Add(note_window_box);
 
+	//Editing song window
+	auto song_window_scr_box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL);
+	auto space = sfg::Label::Create(" ");
+	song_window_scr_box->Pack(space, true, true);
+	auto song_path = sfg::Label::Create("	C:/Music/MIPT/Main_Theme.wav");
+	song_path->SetRequisition(sf::Vector2f(250.0f, .0f));
+	song_window_scr_box->Pack(song_path, true, true);
+	auto song_separator = sfg::Separator::Create(sfg::Separator::Orientation::HORIZONTAL);
+	song_window_scr_box->Pack(song_separator, true, true);
+
+	auto song_window_scr = sfg::ScrolledWindow::Create();
+	song_window_scr->SetScrollbarPolicy( sfg::ScrolledWindow::HORIZONTAL_AUTOMATIC | sfg::ScrolledWindow::VERTICAL_AUTOMATIC );
+	song_window_scr->SetRequisition(SONG_WINDOW_SCR_REQ);
+	song_window_scr->AddWithViewport(song_window_scr_box);
+
+	auto song_window_box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL);
+	song_window_box->Pack(song_window_scr, false, true);
+
+	auto song_window = sfg::Window::Create();
+	song_window->SetTitle("Song Display");
+	song_window->SetPosition(SONG_WINDOW_POS);
+	song_window->SetRequisition(SONG_WINDOW_REQ);
+
+	song_window->Add(song_window_box);
+
+	//Editing main window
 	auto box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL);
 
 	auto label = sfg::Label::Create("  Please type your file path:");
@@ -76,29 +107,36 @@ int main() {
 	auto entry = sfg::Entry::Create("File path...");
 	entry->SetRequisition(ENTRY_REQ);
 
-	auto start_button = sfg::Button::Create( " START " );
-	auto cancel_button = sfg::Button::Create( " CANCEL " );
+	auto start_button = sfg::Button::Create("    START    ");
+	auto cancel_button = sfg::Button::Create("   CANCEL   ");
+	auto getimage_button = sfg::Button::Create(" GET IMAGE ");
 	desktop.SetProperty("Button", "FontSize", BUTTON_FONT_SIZE);
 
+	auto fixed_b = sfg::Fixed::Create();
+	fixed_b->Put(start_button, START_COORD);
+	fixed_b->Put(cancel_button, CANCEL_COORD);
+	fixed_b->Put(getimage_button, GETIMAGE_COORD);
 
-	auto fixed = sfg::Fixed::Create();
-	fixed->Put(start_button, START_CORD);
-	fixed->Put(cancel_button, CANCEL_CORD);
-	fixed->Put(logo_image, LOGO_CORD);
+	auto fixed_img = sfg::Fixed::Create();
+	fixed_img->Put(logo_image, LOGO_COORD);
 
-	table->Attach(entry, sf::Rect<sf::Uint32>(1, 3, 1, 1), sfg::Table::EXPAND, sfg::Table::EXPAND, sf::Vector2f(.0f, .0f));
-	table->Attach(label, sf::Rect<sf::Uint32>(1, 2, 1, 1), sfg::Table::EXPAND, sfg::Table::EXPAND, sf::Vector2f(.0f, .0f));
-	table->Attach(fixed, sf::Rect<sf::Uint32>(2, 1, 1, 40), sfg::Table::EXPAND, sfg::Table::EXPAND, sf::Vector2f(.0f, .0f));
+	//Creating table for displaying elements on certain places
+	table->Attach(entry, sf::Rect<sf::Uint32>(1, 3, 2, 1), sfg::Table::EXPAND, sfg::Table::EXPAND, sf::Vector2f(.0f, .0f));
+	table->Attach(label, sf::Rect<sf::Uint32>(1, 2, 2, 1), sfg::Table::EXPAND, sfg::Table::EXPAND, sf::Vector2f(.0f, .0f));
+	table->Attach(fixed_img, sf::Rect<sf::Uint32>(3, 1, 1, 10), sfg::Table::EXPAND, sfg::Table::EXPAND, sf::Vector2f(.0f, .0f));
+	table->Attach(fixed_b, sf::Rect<sf::Uint32>(2, 11, 2, 30), sfg::Table::EXPAND, sfg::Table::EXPAND, sf::Vector2f(.0f, .0f));
 
 	box->Pack(table);
-
 
 	window->Add(box);
 	window->SetRequisition(WINDOW_SIZE);
 
+	//Displaying everything
 	desktop.Add(note_window);
+	desktop.Add(song_window);
 	desktop.Add(window);
 	desktop.BringToFront(note_window);
+	desktop.BringToFront(song_window);
 
 	sf::Clock clock;
 	// Start the game loop
@@ -106,10 +144,11 @@ int main() {
 		// Process events
 		sf::Event event;
 
-		while ( app_window.pollEvent( event ) ) {
-			// Handle events
-			window->HandleEvent( event );
-			note_window->HandleEvent( event );
+		while ( app_window.pollEvent(event)) {
+			// Handle events (setting windows for being active)
+			window->HandleEvent(event);
+			note_window->HandleEvent(event);
+			song_window->HandleEvent(event);
 
 			// Close window : exit
 			if ( event.type == sf::Event::Closed ) {
@@ -121,9 +160,9 @@ int main() {
 		// pass 0 seconds to the update method.
 		float dt = clock.restart().asSeconds();
 		desktop.Update(dt);
-		note_window->Update(dt);
+		//note_window->Update(dt);
 		app_window.clear();
-		sfgui.Display( app_window );
+		sfgui.Display(app_window);
 		app_window.display();
 	}
 
