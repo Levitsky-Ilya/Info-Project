2528d854dcfdcd89a699055184a43c157632454b
Irina Nikulina <washburn12560@gmail.com>
2016-03-25 00:37:41 +0300

    fft algorithm + window fuction
    the code transforms array  of complex data into array of doubles
    (amplitudes) using fft and window function (Hamming)

---
+++ fftAlgorithm/fft.cpp
@@ -0,0 +1,134 @@
+//   fft.cpp - impelementation of class
+//   of fast Fourier transform - FFT
+//
+//   The code is property of LIBROW
+//   You can use it on your own
+//   When utilizing credit LIBROW site
+
+//   Include declaration file
+//#include "fft.h"
+//   Include math library
+#include <math.h>
+#include "complex.h"
+#include "fft.h"
+
+//   FORWARD FOURIER TRANSFORM
+//     Input  - input data
+//     Output - transform result
+//     N      - length of both input data and result
+/*
+bool CFFT::Forward(const complex *const Input, complex *const Output, const unsigned int N)
+{
+	//   Check input parameters
+	if (!Input || !Output || N < 1 || N & (N - 1))
+		return false;
+	//   Initialize data
+	Rearrange(Input, Output, N);
+	//   Call FFT implementation
+	Perform(Output, N);
+	//   Succeeded
+	return true;
+}
+*/
+
+//   FORWARD FOURIER TRANSFORM, INPLACE VERSION
+//     Data - both input data and output
+//     N    - length of input data
+bool CFFT::fftAlgorithm(complex *const Input, double *const Output, const unsigned int N)
+{
+	//   Check input parameters
+    if (!Input || !Output || N < 1 || N & (N - 1))
+		return false;
+	//   Rearrange
+    complex fftOutput[N];
+
+    Rearrange(Input, fftOutput, N);
+	//   Call FFT implementation
+
+    Perform(fftOutput, N);
+    countAmplitude(fftOutput, Output, N);
+
+	//   Succeeded
+	return true;
+}
+
+//
+void CFFT::countAmplitude(complex *const Input, double *const Output, const unsigned int N)
+{
+    unsigned int i = 0;
+    for (i = 0; i < N; i++) {
+            Output[i] = Input[i].countAmplitude(); //Fill amplitude array with data
+        }
+
+}
+
+//   Rearrange function
+void CFFT::Rearrange(const complex *const Input, complex *const Output, const unsigned int N)
+{
+	//   Data entry position
+	unsigned int Target = 0;
+	//   Process all positions of input signal
+	for (unsigned int Position = 0; Position < N; ++Position)
+	{
+		//  Set data entry
+		Output[Target] = Input[Position];
+		//   Bit mask
+		unsigned int Mask = N;
+		//   While bit is set
+		while (Target & (Mask >>= 1))
+			//   Drop bit
+			Target &= ~Mask;
+		//   The current bit is 0 - set it
+		Target |= Mask;
+	}
+}
+
+
+//   FFT implementation
+void CFFT::Perform(complex *const Data, const unsigned int N, const bool Inverse /* = false */)
+{
+	const double pi = Inverse ? 3.14159265358979323846 : -3.14159265358979323846;
+	//   Iteration through dyads, quadruples, octads and so on...
+	for (unsigned int Step = 1; Step < N; Step <<= 1)
+	{
+		//   Jump to the next entry of the same transform factor
+		const unsigned int Jump = Step << 1;
+		//   Angle increment
+		const double delta = pi / double(Step);
+		//   Auxiliary sin(delta / 2)
+		const double Sine = sin(delta * .5);
+		//   Multiplier for trigonometric recurrence
+		const complex Multiplier(-2. * Sine * Sine, sin(delta));
+		//   Start value for transform factor, fi = 0
+		complex Factor(1.);
+		//   Iteration through groups of different transform factor
+		for (unsigned int Group = 0; Group < Step; ++Group)
+		{
+			//   Iteration within group 
+			for (unsigned int Pair = Group; Pair < N; Pair += Jump)
+			{
+				//   Match position
+				const unsigned int Match = Pair + Step;
+				//   Second term of two-point transform
+				const complex Product(Factor * Data[Match]);
+				//   Transform for fi + pi
+				Data[Match] = Data[Pair] - Product;
+				//   Transform for fi
+				Data[Pair] += Product;
+			}
+			//   Successive transform factor via trigonometric recurrence
+			Factor = Multiplier * Factor + Factor;
+		}
+	}
+}
+
+//   Scaling of inverse FFT result
+void CFFT::Scale(complex *const Data, const unsigned int N)
+{
+	const double Factor = 1. / double(N);
+	//   Scale all data entries
+	for (unsigned int Position = 0; Position < N; ++Position)
+		Data[Position] *= Factor;
+}
+
+
