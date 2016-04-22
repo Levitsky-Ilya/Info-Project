cb8937ef9b9d5a7385efad3959c28d903c1623ab
Alexander Surchenko <surchenko.a@mail.ru>
2016-04-22 19:55:24 +0300

    Adding full range of notes

--- Interface/Develop/note_generator/main.cpp
+++ Interface/Develop/note_generator/main.cpp
@@ -89,18 +89,34 @@ void drawNote(vector<struct Notes> & queue, ofstream & f)
 
 	map<int, string> freqMap;
 
-	freqMap[25] =    "c"; freqMap[26] =  "cis";
-	freqMap[27] =    "d"; freqMap[28] =  "dis";
-	freqMap[29] =    "e"; freqMap[30] =    "f";
-	freqMap[31] =  "fis"; freqMap[32] =    "g";
-	freqMap[33] =  "gis"; freqMap[34] =    "a";
-	freqMap[35] =  "ais"; freqMap[36] =    "b";
-	freqMap[37] =   "c'"; freqMap[38] = "cis'";
-	freqMap[39] =   "d'"; freqMap[39] = "dis'";
-	freqMap[40] =   "e'"; freqMap[41] =   "f'";
-	freqMap[42] = "fis'"; freqMap[43] =   "g'";
-	freqMap[44] = "gis'"; freqMap[45] =   "a'";
-	freqMap[46] = "ais'"; freqMap[47] =   "b'";
+	freqMap[0]  =     "c,,"; freqMap[1]  =   "cis,,"; freqMap[2]  =     "d,,";
+	freqMap[3]  =   "dis,,"; freqMap[4]  =     "e,,"; freqMap[5]  =     "f,,";
+	freqMap[6]  =   "fis,,"; freqMap[7]  =     "g,,"; freqMap[8]  =   "gis,,";
+	freqMap[9]  =     "a,,"; freqMap[10] =   "ais,,"; freqMap[11] =     "b,,";
+	freqMap[12] =      "c,"; freqMap[13] =    "cis,"; freqMap[14] =      "d,";
+	freqMap[15] =    "dis,"; freqMap[16] =      "e,"; freqMap[17] =      "f,";
+	freqMap[18] =    "fis,"; freqMap[19] =      "g,"; freqMap[20] =    "gis,";
+	freqMap[21] =      "a,"; freqMap[22] =    "ais,"; freqMap[23] =      "b,";
+	freqMap[24] =       "c"; freqMap[25] =     "cis"; freqMap[26] =       "d";
+	freqMap[27] =     "dis"; freqMap[28] =       "e"; freqMap[29] =       "f";
+	freqMap[30] =     "fis"; freqMap[31] =       "g"; freqMap[32] =     "gis";
+	freqMap[33] =       "a"; freqMap[34] =     "ais"; freqMap[35] =       "b";
+	freqMap[36] =      "c'"; freqMap[37] =    "cis'"; freqMap[38] =      "d'";
+	freqMap[39] =    "dis'"; freqMap[40] =      "e'"; freqMap[41] =      "f'";
+	freqMap[42] =    "fis'"; freqMap[43] =      "g'"; freqMap[44] =    "gis'";
+	freqMap[45] =      "a'"; freqMap[46] =    "ais'"; freqMap[47] =      "b'";
+	freqMap[48] =     "c''"; freqMap[49] =   "cis''"; freqMap[50] =     "d''";
+	freqMap[51] =   "dis''"; freqMap[52] =     "e''"; freqMap[53] =     "f''";
+	freqMap[54] =   "fis''"; freqMap[55] =     "g''"; freqMap[56] =   "gis''";
+	freqMap[57] =     "a''"; freqMap[58] =   "ais''"; freqMap[59] =     "b''";
+	freqMap[60] =    "c'''"; freqMap[61] =  "cis'''"; freqMap[62] =    "d'''";
+	freqMap[63] =  "dis'''"; freqMap[64] =    "e'''"; freqMap[65] =    "f'''";
+	freqMap[66] =  "fis'''"; freqMap[67] =    "g'''"; freqMap[68] =  "gis'''";
+	freqMap[69] =    "a'''"; freqMap[70] =  "ais'''"; freqMap[71] =    "b'''";
+	freqMap[72] =   "c''''"; freqMap[73] = "cis''''"; freqMap[74] =   "d''''";
+	freqMap[75] = "dis''''"; freqMap[76] =   "e''''"; freqMap[77] =   "f''''";
+	freqMap[78] = "fis''''"; freqMap[79] =   "g''''"; freqMap[80] = "gis''''";
+	freqMap[81] =   "a''''"; freqMap[82] = "ais''''"; freqMap[83] =   "b''''";
 
 	struct NotePause {
 		float pauseDur;
